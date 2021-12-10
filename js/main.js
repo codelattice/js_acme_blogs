001a493196f038d56b4aea03993069fe5500885b
codelattice <89589147+codelattice@users.noreply.github.com>
2021-12-10 00:01:57 -0500

    first try

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function createElemWithText(){
+
+}
+
+function createSelectOptions(){
+
+}
+
+function toggleCommentSection(){
+
+}
+
+function toggleCommentButton(){
+
+}
+
+function deleteChildElements(){
+
+
+}
+
+function addButtonListeners(){
+
+}
+
+function removeButtonListeners(){
+
+}
+
+function createComments(){
+
+}
+
+function populateSelectMenu(){
+    
+}
+
+function getUsers(){
+
+}
+
+function getUserPosts(){
+
+}
+
+function getUser(){
+    
+}
