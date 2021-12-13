609fc6c7b1abd222b4d4f7d49628ac48f6c73d99
codelattice <89589147+codelattice@users.noreply.github.com>
2021-12-13 10:33:01 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -109,7 +109,15 @@ const getUser = async (userId) => {
 }
 
 
-function getPostComments(){
+const getPostComments = async(postId) => {
+    try{    
+        if (!postId) return;
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts/${postId}');
+        if (!res.ok) throw new Error("Status code not in 200-299 range");
+        return await res.json();
+    } catch(err){
+        console.error(err);
+    }
     
 }
 
