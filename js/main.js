088d50aad3fd63bf9315462190f107bc99ad7f1c
codelattice <89589147+codelattice@users.noreply.github.com>
2021-12-13 11:42:35 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -67,15 +67,16 @@ function createComments(param){
 
     if (!param) return;
     let frag = document.createDocumentFragment();
-    for (let i = 0; i < param.length(); i++){
+    param.forEach((param) => {
         const rtickle = document.create(article);
-        const h3Element = createElemWithText('p', comment.body);
+        const h3Element = createElemWithText('h3', comment.name);
+        const paraElem1 = createElemtWithText('p', coment.body)
         const paraElem = createElemWithText('h3', 'From: ' + param);
         rtickle.append(h3Element);
         rtickle.append(paraElem);
         frag.append(rtickle);
-        return frag;
-    }
+    });
+    return frag;
 }
 
 function populateSelectMenu(){
