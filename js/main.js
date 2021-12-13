78ceedc7768e59eb1236eb9c145f00928ddc3bc9
codelattice <89589147+codelattice@users.noreply.github.com>
2021-12-13 15:25:13 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -191,7 +191,7 @@ const selectMenuChangeEventHandler = async(event) =>{
     let err_ray = [];
     let refreshPostsArray = 1; //review and change implementation
     let userID = event?.target?.value;
-    const posts = await getUserPosts(userID);
+    const posts = []; //await getUserPosts(userID);
     
     err_ray.push(refreshPostsArray);
     err_ray.push(posts);
