4991fd8848080907d2e659b935a955c5fa9472a6
codelattice <89589147+codelattice@users.noreply.github.com>
2021-12-13 20:20:09 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ function deleteChildElements(parentElement){
 
 function addButtonListeners(){
 
-    const buttons = document.querySelectorAll('buttons');
+    const buttons = document.querySelectorAll('button');
     //buttons.addEventListener("click", function (e){toggleComments(e, postId)}, false);
     return buttons;
 
